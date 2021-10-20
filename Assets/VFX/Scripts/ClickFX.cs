f0ac89d004c4769c990d0eb494e9536e32bfeee6
inneswhite <47889269+inneswhite@users.noreply.github.com>
2021-10-20 11:13:42 +0100

    Added Click VFX to Player
    Changed to Linear Colour Space
    Enabled Opaque and Depth Textures
    Added Click VFX to Player Prefab

--- Assets/VFX/Scripts/ClickFX.cs
+++ Assets/VFX/Scripts/ClickFX.cs
@@ -6,7 +6,7 @@ public class ClickFX : MonoBehaviour
 {
     [Header("Properties")]
     [SerializeField]
-    GameObject clickRadiusMeshObj;
+    GameObject clickRadiusMeshObj, groundUIObj;
 
     [SerializeField]
     float clickRadius = 3f;
@@ -28,6 +28,9 @@ public class ClickFX : MonoBehaviour
 
 
     Material clickRadiusMaterial;
+    Material groundUIMaterial;
+    float groundUIAlpha;
+    float groundUIMaxAlpha;
     Color clickRadiusColor;
 
     enum ClickState
@@ -39,27 +42,43 @@ public class ClickFX : MonoBehaviour
 
     ClickState clickState = ClickState.inactive;
 
+    bool isActive = false;
+
 
     private void Start()
     {
         clickRadiusMeshObj = Instantiate(clickRadiusMeshObj);
         clickRadiusMeshObj.transform.localScale = Vector3.one * 0.1f;
         clickRadiusMaterial = clickRadiusMeshObj.GetComponent<MeshRenderer>().material;
-        clickRadiusColor = clickRadiusMaterial.color;
+        clickRadiusColor = clickRadiusMaterial.GetColor("_Tint");
+        maxAlpha = clickRadiusMaterial.GetFloat("_Alpha");
         alpha = maxAlpha;
 
+        groundUIMaterial = groundUIObj.GetComponent<MeshRenderer>().material;
+        groundUIMaxAlpha = groundUIMaterial.GetFloat("_Alpha");
+        groundUIAlpha = groundUIMaxAlpha;
+        
+
+        groundUIObj.SetActive(false);
         clickRadiusMeshObj.SetActive(false);
     }
 
 
     public void Activate()
     {
-        clickRadiusMeshObj.SetActive(true);
+        if (!isActive)
+        {
+            clickRadiusMeshObj.SetActive(true);
+            groundUIObj.SetActive(true);
 
-        ClickGrowLeanTween();
-        clickRadiusMeshObj.transform.position = transform.position;
+            ClickGrowLeanTween();
+            GroundUIFadeOut();
+            clickRadiusMeshObj.transform.position = transform.position;
+            isActive = true;
+        }
     }
 
+    #region ClickRefractionAnimation
     void ClickGrowLeanTween()
     {
         if (clickState == ClickState.inactive)
@@ -73,9 +92,10 @@ public class ClickFX : MonoBehaviour
     {
         if (clickState == ClickState.growing)
         {
-            LeanTween.value(clickRadiusMeshObj, maxAlpha, 0f, 1f).setOnUpdate((alpha) => {
-                clickRadiusMaterial.color = new Color(clickRadiusColor.r, clickRadiusColor.g, clickRadiusColor.b, alpha);
-            }).setDelay(fadeOutDelay).setOnComplete(AnimationComplete);
+            LeanTween.value(clickRadiusMeshObj, maxAlpha, 0f, fadeOutDuration).setOnUpdate((alpha) =>
+            {
+                clickRadiusMaterial.SetFloat("_Alpha", alpha);
+            }).setDelay(fadeOutDelay).setOnComplete(AnimationComplete).setEaseOutQuad(); ;
             clickState = ClickState.fadeingOut;
         }
     }
@@ -85,10 +105,26 @@ public class ClickFX : MonoBehaviour
         
         clickState = ClickState.inactive;
         clickRadiusMeshObj.transform.localScale = Vector3.one * 0.1f;
-        clickRadiusMaterial.color = new Color(clickRadiusColor.r, clickRadiusColor.g, clickRadiusColor.b, maxAlpha);
+        clickRadiusMaterial.SetFloat("_Alpha", maxAlpha);
 
+        isActive = false;
         clickRadiusMeshObj.SetActive(false);
         
+    }
+    #endregion
+
+    void GroundUIFadeOut()
+    {
+        LeanTween.value(groundUIObj, groundUIMaxAlpha, 0f, fadeOutDuration * 0.5f).setOnUpdate((groundUIAlpha) =>
+        {
+            groundUIMaterial.SetFloat("_Alpha", groundUIAlpha);
+        }).setDelay(fadeOutDelay).setEaseOutQuad().setOnComplete(GroundUIAnimationComplete);
+    }
+
+    void GroundUIAnimationComplete()
+    {
+        groundUIMaterial.SetFloat("_Alpha", groundUIMaxAlpha);
+        groundUIObj.SetActive(false);
     }
 
     private void Update()
