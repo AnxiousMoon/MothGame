7a31be4c87cb148f3ce5b1f2697ac79499bd4e64
AnxiousMoon <seversonelena@gmail.com>
2021-10-22 10:38:25 +0100

    Clean up on webs

---
+++ Assets/Scripts/Floaty.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floaty : MonoBehaviour {
+
+	private Vector3 newPosition;
+	private Rigidbody rdb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		newPosition = transform.position;
+    	newPosition.y += Mathf.Sin(2.5f * Time.time) * 1f * Time.deltaTime;
+   		transform.position = newPosition;
+
+   		transform.Rotate (1.5f, 0f, 0f * Time.deltaTime);
+	}	
+
+}
