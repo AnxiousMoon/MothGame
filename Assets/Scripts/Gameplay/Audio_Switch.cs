a5d023d9edbab0da2060c531d24a2977520fb8bb
robbiepeden <robbiepeden1999@gmail.com>
2021-11-22 11:05:06 +0000

    Audio Switches
    Footsteps will now switch depending on each surface.

---
+++ Assets/Scripts/Gameplay/Audio_Switch.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_Switch : MonoBehaviour
+{
+    public AK.Wwise.Switch Cave;
+    public AK.Wwise.Switch Platform;
+
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Platform.SetValue(Player);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Cave.SetValue(Player);
+    }
+}
