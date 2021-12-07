2bcd1648850d9a22371fd633cb7356ddd7fce2b8
robbiepeden <robbiepeden1999@gmail.com>
2021-12-07 17:32:22 +0000

    Footstep bug Fix
    Footstep bug is fixed.

--- Assets/Scripts/Gameplay/Audio_Switch_Wood.cs
+++ Assets/Scripts/Gameplay/Audio_Switch_Wood.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class Audio_Switch_Wood : MonoBehaviour
 {
     public AK.Wwise.Switch Platform;
+    public AK.Wwise.Switch Cave;
 
     public GameObject Player;
 
     private void OnTriggerEnter(Collider col)
     {
-        if (col.tag == "Player");
+        if(col.tag == "Player" || col.tag == "Dashing")
         {
             Platform.SetValue(Player);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player" || col.tag == "Dashing")
+        {
+            Cave.SetValue(Player);
         }
 
     }
